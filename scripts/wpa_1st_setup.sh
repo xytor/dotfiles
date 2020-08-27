0a83f2b21493a5df437e147cb4b9a99f20635f90
xytor <TheMeiXer@gmail.com>
2020-08-27 21:08:43 +0200

    wpa_1st_setup

---
+++ scripts/wpa_1st_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo "Write your interface"
+read interface
+touch /etc/wpa_supplicant/wpa_supplicant-$interface.conf
+echo "ctrl_interface=/run/wpa_supplicant" >> /etc/wpa_supplicant/wpa_supplicant-$interface.conf
+echo "update_config=1" >> /etc/wpa_supplicant/wpa_supplicant-$interface.conf
+echo "Write your essid"
+read essid
+echo "Write password"
+read -s passwd
+wpa_passphrase $essid $passwd >> /etc/wpa_supplicant/wpa_supplicant-$interface.conf
+wpa_supplicant -B -i $interface -c /etc/wpa_supplicant/wpa_supplicant-$interface.conf
+echo "All done!"
