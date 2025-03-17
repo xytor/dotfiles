4eb5fcd0cbbf5525130d5cf3614f47c043ac875a
xytor <63665198+xytor@users.noreply.github.com>
2025-03-17 14:34:15 +0100

    Update wpa_1st_setup.sh

--- scripts/wpa_1st_setup.sh
+++ scripts/wpa_1st_setup.sh
@@ -4,8 +4,8 @@ read interface
 touch /etc/wpa_supplicant/wpa_supplicant-$interface.conf
 echo "ctrl_interface=/run/wpa_supplicant" >> /etc/wpa_supplicant/wpa_supplicant-$interface.conf
 echo "update_config=1" >> /etc/wpa_supplicant/wpa_supplicant-$interface.conf
-echo "Write your essid"
-read essid
+echo "Write your ssid"
+read ssid
 echo "Write password"
 read -s passwd
 wpa_passphrase $essid $passwd >> /etc/wpa_supplicant/wpa_supplicant-$interface.conf
